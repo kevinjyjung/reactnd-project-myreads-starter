e906ab93509b1e20d433142e808ceec2e149e0c3
Kevin Jung <jjun3170@uw.edu>
2017-08-02 02:23:47 -0700

    React Router used to replace existing navigation. Book component added.

---
+++ src/Book.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './App.css'
+
+class Book extends Component {
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: this.props.backgroundImage
+            }}>
+          </div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.authors}</div>
+      </div>
+    );
+  }
+}
+
+Book.propTypes = {
+  backgroundImage: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.string.isRequired
+}
+
+export default Book;
